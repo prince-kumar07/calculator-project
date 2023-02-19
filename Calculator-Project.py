bc5c529f9197b9af2fc2deba8efe6f79484cb039
prince-kumar07 <125768513+prince-kumar07@users.noreply.github.com>
2023-02-19 17:45:58 +0530

    Add files via upload

---
+++ Calculator-Project.py
@@ -0,0 +1,32 @@
+# Take input for operand
+Num1 = input('Enter Tne First Number: ')
+Operator = input('Enter Tne  Operator: ')
+Num2 = input('Enter Tne Second Number: ')
+
+
+# lets print the type of the variables
+#print(type(Num1), type(Num2))
+
+# Converting the type of variables
+Num1 = int(Num1)
+Num2 = int(Num2)
+
+# lets print type of the variables
+#print(type(Num1), type(Num2))
+
+#lets check the operator first
+result = ''
+if Operator == '+':
+  result = Num1 + Num2
+elif Operator == '-':
+  result = Num1 - Num2
+elif Operator == '*':
+  result = Num1 * Num2 
+elif Operator == '/':
+  result = Num1 / Num2 
+else:
+  result = 'Not Defined'
+
+#lets print it
+
+print('The Result Is : ', result)
